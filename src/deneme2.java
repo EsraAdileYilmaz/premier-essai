1bf013386306679e3feb67e31a0d2b01f0e78eac
EsraAdileYilmaz <yilmazesraadile@gmail.com>
2023-09-28 16:13:43 +0200

    deneme2

---
+++ src/deneme2.java
@@ -0,0 +1,7 @@
+public class deneme2 {
+    public static void main(String[] args) {
+
+        System.out.println("bu bir ekleme dosyasidir.");
+        System.out.println("bu dosya update edilecek. ");
+    }
+}
